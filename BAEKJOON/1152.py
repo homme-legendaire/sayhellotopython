a9310b5d8f9c2b4f45ef359ef0da14518b449feb
sooyoung <itsurboiace00@naver.com>
2022-03-16 14:51:10 +0900

    [BAEKJOON] 03.16 (WED)

--- BAEKJOON/1152.py
+++ BAEKJOON/1152.py
@@ -1,9 +1,2 @@
-str = input()
-cnt = 0
-if str[0] != ' ':
-    cnt += 1
-for i in range(len(str)-1):
-    if str[i] == ' ':
-        if str[i+1] != ' ':
-            cnt += 1
-print(cnt)
+list = input().split()
+print(len(list))
