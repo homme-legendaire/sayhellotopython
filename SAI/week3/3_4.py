cd1a18fea8973332a85536e6f3c5a82d75f56b0f
sooyoung <itsurboiace00@naver.com>
2022-03-20 14:31:50 +0900

    [SAI] 3주차 문제풀이

--- SAI/week3/3_4.py
+++ SAI/week3/3_4.py
@@ -1,6 +1,7 @@
 import numpy as np
 a = int(input())
 arr = np.random.randint(1, 1000, size=a)
+arr = np.sort(arr)
 print(arr)
 result = np.empty((0, 4), dtype=np.int32)
 delete = np.empty((0, 4), dtype=np.int32)
@@ -10,8 +11,8 @@ while i <= a-3:
     cnt += 1
     result = np.append(result, arr[i:i+4, ])
     i += 2
-    if i+4 > a and a % 4 != 0:
-        delete = np.append(delete, arr[i:i+3, ])
+    if i+4 >= a-1 and i % 4 != 0:
+        delete = np.append(delete, arr[i+2:i+5, ])
         break
 print(result.reshape(cnt, 4))
 print(f"삭제된 원소:{delete}")
