a05e2c0cc6d61f1f3adff127f22fc9a990928275
sooyoung <itsurboiace00@naver.com>
2022-03-28 23:28:00 +0900

    [SAI] 03.28 (MON)

---
+++ pandas_study/2/drop_duplicate.py
@@ -0,0 +1,19 @@
+import pandas as pd
+student_list = [{'name': 'John', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Nate', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Abraham', 'major': "Physics", 'sex': "male"},
+                {'name': 'Brian', 'major': "Psychology", 'sex': "male"},
+                {'name': 'Janny', 'major': "Economics", 'sex': "female"},
+                {'name': 'Yuna', 'major': "Economics", 'sex': "female"},
+                {'name': 'Jeniffer', 'major': "Computer Science", 'sex': "female"},
+                {'name': 'Edward', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Zara', 'major': "Psychology", 'sex': "female"},
+                {'name': 'Wendy', 'major': "Economics", 'sex': "female"},
+                {'name': 'Sera', 'major': "Psychology", 'sex': "female"},
+                {'name': 'John', 'major': "Computer Science", 'sex': "male"},
+                ]
+df = pd.DataFrame(student_list, columns=['name', 'major', 'sex'])
+print(df)
+print(df.duplicated())
+print(df.drop_duplicates(['name']))
+print(df.drop_duplicates(['name'], keep='last'))
